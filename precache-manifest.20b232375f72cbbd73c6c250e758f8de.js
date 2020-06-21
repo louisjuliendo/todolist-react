a39c5f65a96473ff2cf86d17252036ad9b03a24e
Louis Juliendo <louisjuliendo@gmail.com>
2020-06-21 21:06:02 +0700

    Updates

---
+++ precache-manifest.20b232375f72cbbd73c6c250e758f8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c08fed9ecf363cce61b056ca139a4e7",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "4c4d286f259cb8d56316",
+    "url": "/todolist-react/static/css/main.8b928e93.chunk.css"
+  },
+  {
+    "revision": "1e90d2a16dbbf07a38ef",
+    "url": "/todolist-react/static/js/2.dee59fe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.dee59fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4d286f259cb8d56316",
+    "url": "/todolist-react/static/js/main.106da49f.chunk.js"
+  },
+  {
+    "revision": "55364f13aeae8b1110bf",
+    "url": "/todolist-react/static/js/runtime-main.7c96e71d.js"
+  }
+]);
